8ac6336e93b04973b40b737ba4f8d65c9bc49b24
Aaryav Munjal <aaryav.munjal@gmail.com>
2025-04-17 11:24:14 +0530

    pure_components

---
+++ src/02-pure-components/ChildRegular.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ChildRegular=({name})=>{
+  console.log('ChildRegular component rendered');
+  return<p>ChildRegular : Name is {name}</p>
+
+}
+export default ChildRegular;
